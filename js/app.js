f7112c828044be9f21a224dcf1695f5266ab89a1
James M <saintjames@MacBook-Pro-2.local>
2018-08-15 21:24:29 -0600

    pokemon project

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-Helpful tip for todays homework
-
-
-// make game set up 3 cards vs 3 cards, after round, start new round with new cards until there // are less than 6 cards total
-
-class Cards = [
+const cards = [
 {name: "Bulbasaur", damage:60}, 
 {name: "Caterpie", damage:40},
 {name: "Charmander", damage:60},
@@ -21,5 +16,103 @@ class Cards = [
 {name: "Rattata", damage:30}, 
 {name: "Squirtle", damage:60}, 
 {name: "Vulpix", damage:50}, 
-{name: "Weedle", damage:40}
+{name: "Weedle", damage:40},
 ]
+
+const playerOne = {
+	points: 0,
+	battlesWon: 0,
+	handOne: []
+};
+const playerTwo = {
+	points: 0,
+	battlesWon: 0,
+	handTwo: []
+};
+
+const game = {
+	library: cards,
+	gameBoard: [],
+	graveYard: [],
+	currentRound: 0,
+	shuffle(){
+		let shuffle = Math.floor(Math.random() * this.library.length);
+		let drawnCard = this.library.splice(shuffle, 1)[0];
+		return drawnCard;
+	},
+	deal(){
+		for(let i = 0; i < 3; i++){
+			let dealPlayerOne = this.shuffle();
+			playerOne.handOne.push(dealPlayerOne);
+			let dealPlayerTwo = this.shuffle();
+			playerTwo.handTwo.push(dealPlayerTwo);
+		}
+	},
+	fight(){
+		this.currentRound++
+		console.log("ROUND " + this.currentRound)
+		let cardOne = playerOne.handOne.splice(0,1)[0];
+		let cardTwo = playerTwo.handTwo.splice(0,1)[0];
+		console.log("Player One Chose " + cardOne.name);
+		console.log("Player Two sent out " + cardTwo.name);
+		if(cardOne.damage > cardTwo.damage){
+			playerOne.points++
+			console.log("Player One Won the Battle!")
+			console.log("Player One has " + playerOne.points + " Points!")
+		} else if(cardOne.damage < cardTwo.damage) {
+			playerTwo.points++
+			console.log("Player Two Won the Battle");
+			console.log("Player Two has " + playerTwo.points + " Points!")
+		} else {
+			console.log("TIE!");
+		}
+		this.graveYard.push(this.cardOne);
+		this.graveYard.push(this.cardTwo);
+		console.log("/////////////////////////")
+	},
+	chooseWinner(){
+		if(playerOne.points > playerTwo.points){
+			console.log("Player One has won the Battle!")
+			playerOne.battlesWon++
+		} else {
+			console.log("Player Two has won the Battle!")
+			playerTwo.battlesWon++
+		}
+	},
+	startGame(){
+		this.deal()
+		this.fight()
+		this.fight()
+		this.fight()
+		this.chooseWinner()
+	}
+};
+
+game.startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
